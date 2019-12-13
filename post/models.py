0610d95cffceddd2b799f75e73e491266c692dbf
Jill Pesce <jillpesce44@gmail.com>
2019-12-12 21:54:11 -0500

    requirements.txt

--- post/models.py
+++ post/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 class Post(models.Model):
-	id = models.IntegerField(max_length=200, primary_key=True)
 	title = models.CharField(max_length=200)
 	description = models.TextField()
 	image = models.CharField(max_length=200)
 	author = models.CharField(max_length=200)
 	date = models.CharField(max_length=200)
+
+
+	def __str__(self):
+		return self.title
